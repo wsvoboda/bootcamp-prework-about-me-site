e9359e4ab8897fbc11574350b3bd58e29df1632d
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-03-01 17:33:45 -0600

    Add files via upload

---
+++ about-me-site/scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/gabby.jpeg') {
+      myImage.setAttribute('src','images/norway.jpg');
+    } else {
+      myImage.setAttribute('src','images/gabby.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Welcome to the page, ${myName}!`;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the page, ' + storedName + '!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
